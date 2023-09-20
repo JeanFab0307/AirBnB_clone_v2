5ee648324448889e64e8337afe0b8f5c412d4604
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-09-20 16:11:03 +0300

    Correct syntax

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- creates DB hbnb_test_db and user hbnb_test in localhost
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localthost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localthost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localthost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
