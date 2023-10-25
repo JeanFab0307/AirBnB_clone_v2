3bfe0dbdc825e28d0a199b1d02d1198807998f08
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-25 14:55:53 +0300

    Change to capital C

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     newText = text.replace('_', ' ')
-    return 'c {}'.format(newText)
+    return 'C {}'.format(newText)
 
 
 if __name__ == '__main__':
