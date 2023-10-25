79915694910f9cd60eac91de7b24e66687d7f58c
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-25 13:31:32 +0300

    Changes some pathnames

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Hello World" > /data/web_static/releases/test/index.html
-ln -f /data/web_static/current /data/web_static/releases/test/
+ln -sf /data/web_static/test /data/web_static/releases/current/
 sudo chown -hR ubuntu:ubuntu /data
 sudo sed - i '/sever_name _;/a\location /hbnb_static{\n\talias /data/web_static/current/;/n/t}' /etc/nginx/sites-available/default
 sudo service nginx start
