d698db5378a42ccb28a044369d6477e03e8f2791
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-25 10:12:22 +0300

    Add flags and correct content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Hello World" > /data/web_static/releases/test/index.html
 ln -f /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed - i '/sever_name _;/a\location /hbnb_static{\n\talias /data/web_static/current/n/t}' /etc/nginx/sites-enabled/default
+sudo chown -hR ubuntu:ubuntu /data
+sudo sed - i '/sever_name _;/a\location /hbnb_static{\n\talias /data/web_static/current/;/n/t}' /etc/nginx/sites-available/default
 sudo service nginx start
 sudo service nginx restart
